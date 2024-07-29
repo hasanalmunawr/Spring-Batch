2ffe0dea6b2ceba5d03c91e5d0813ad0a67c2c41
hasanalmunawr <hasanalmunawar01@gmail.com>
2024-07-29 13:50:43 +0700

    Refactor(config) : add Bean for Write Out

--- src/main/java/com/hasanalmunawr/spring_batch/config/BatchConfig.java
+++ src/main/java/com/hasanalmunawr/spring_batch/config/BatchConfig.java
@@ -11,16 +11,20 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -33,6 +37,7 @@ public class BatchConfig {
 //    private final JobBuilder jobRepository;
 
 
+//    TO READ AND STORE INTO DATABASE
     @Bean
     public FlatFileItemReader<Customer> reader() {
        return new FlatFileItemReaderBuilder<Customer>()
@@ -41,6 +46,18 @@ public class BatchConfig {
                .linesToSkip(1)
                .lineMapper(lineMapper())
                .build();
+    }
+
+//    TO WRITE FROM DATABASE AND RESULT A FILE
+    @Bean
+    public FlatFileItemWriter<Customer> writerOut() {
+        return new FlatFileItemWriterBuilder<Customer>()
+                .name("CustomerItemWriter")
+                .resource(new FileSystemResource("output.csv"))
+                .delimited()
+                .delimiter(",")
+                .names("id", "firstname", "lastname", "email", "gender", "contact", "country", "dob")
+                .build();
     }
 
     @Bean
@@ -54,6 +71,13 @@ public class BatchConfig {
       writer.setRepository(customerRepository);
       writer.setMethodName("save");
       return writer;
+    }
+
+    public RepositoryItemReader<Customer> readerOut() {
+        RepositoryItemReader<Customer> reader = new RepositoryItemReader<>();
+        reader.setRepository(customerRepository);
+        reader.setMethodName("findAll");
+        return reader;
     }
 
     @Bean
@@ -85,9 +109,19 @@ public class BatchConfig {
     }
 
     @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+       return new StepBuilder("step2", jobRepository)
+               .<Customer, Customer> chunk(10, transactionManager)
+               .reader(readerOut())
+               .writer(writerOut())
+               .build();
+    }
+
+    @Bean
     public Job importUserJob(JobRepository jobRepository,Step step1, JobCompletionNotificationListener listener) {
         JobBuilder jobBuilder = new JobBuilder("importUserJob", jobRepository);
         SimpleJobBuilder simpleJobBuilder = jobBuilder.start(step1);
         return simpleJobBuilder.listener(listener).build();
     }
+
 }
